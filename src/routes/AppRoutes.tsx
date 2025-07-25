428a1d04166d61d0d063173e6605522b91678103
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-25 17:04:56 -0300

    feat: product route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router'
 
 import { Home } from '../pages/Home'
+import { Products } from '../pages/Products'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route element={<Home />} index path="/" />
+      <Route element={<Products />} path="/products" />
     </Routes>
   )
 }
