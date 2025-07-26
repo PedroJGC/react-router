ec547570c58191d2ea862a2d03e8184371161a1f
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-26 11:16:31 -0300

    feat: useSearchParams

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,11 +1,22 @@
+import { useSearchParams } from 'react-router'
+
 export function Products() {
+  const [searchParams] = useSearchParams()
+  const category = searchParams.get('category')
+
   return (
     <div>
-      <h1>Products</h1>
-
       <nav>
         <a href="/">Home</a>
       </nav>
+
+      <h1>Products</h1>
+
+      {category && (
+        <span>
+          Category <strong>{category}</strong>
+        </span>
+      )}
     </div>
   )
 }
